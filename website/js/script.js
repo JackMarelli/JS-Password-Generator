e990a101eb24b4fb0bf8846f0575f5c7f9d5ce84
JackMarelli <marelligiacomo@gmail.com>
2021-09-22 19:02:37 +0200

    cominciato js

---
+++ website/js/script.js
@@ -0,0 +1,7 @@
+function genera() {
+    let checkLowercase = document.getElementById("lowercase").checked;
+    let checkUppercase = document.getElementById("uppercase").checked;
+    let checkNumbers = document.getElementById("numbers").checked;
+    
+    
+}
